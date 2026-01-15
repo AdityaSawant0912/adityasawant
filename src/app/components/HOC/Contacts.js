3a0fe396980c052ee113a30b1a76bfde198728b3
Aditya Sawant <arsawant09@gmail.com>
2026-01-15 16:41:41 -0500

    Link to resume updated

--- src/app/components/HOC/Contacts.js
+++ src/app/components/HOC/Contacts.js
@@ -10,7 +10,7 @@ function Contacts() {
             <Contact label="Github" href="https://github.com/AdityaSawant0912">AdityaSawant0912</Contact>
             <Contact label="Linkedin" href="https://www.linkedin.com/in/adityasawant09/">adityasawant09</Contact>
             <Contact label="X (Twitter)" href="https://www.x.com/in/adzzhere/">adzzhere</Contact>
-            <Contact label="Resume" href="/Aditya Sawant Resume.pdf">Aditya Sawant</Contact>
+            <Contact label="Resume" href="https://resume.adityasawant.dev/">Aditya Sawant</Contact>
         </Section>
     )
 }
